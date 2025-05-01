6cdc5d8f87b0f3101b7704045680aa4f301fbc80
Nils Hartmann <nils@nilshartmann.net>
2025-05-01 14:48:43 +0200

    [React2025] INITIAL

--- react2025/src/main.tsx
+++ react2025/src/main.tsx
@@ -1,12 +1,8 @@
 import "./index.css";
 
-import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { createRoot } from "react-dom/client";
 
-import { createQueryClient } from "./create-query-client.ts";
-import { routeTree } from "./routeTree.gen.ts";
+import App from "./App.tsx";
 
 const rootElement = document.getElementById("root");
 
@@ -14,25 +10,4 @@ if (rootElement === null) {
   throw new Error("Cannot find the root element");
 }
 
-const queryClient = createQueryClient();
-
-const router = createRouter({
-  routeTree,
-  context: {
-    queryClient,
-  },
-});
-
-// Register the router instance for type safety
-declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
-}
-
-createRoot(rootElement).render(
-  <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
-    <ReactQueryDevtools />
-  </QueryClientProvider>,
-);
+createRoot(rootElement).render(<App />);
