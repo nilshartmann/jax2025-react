fd41580fd8a30810d9a7d7d93b91e472191d74e2
Nils Hartmann <nils@nilshartmann.net>
2025-05-04 00:23:32 +0200

    [Vitest] 01: Basic Test (ohne Netzwerk und Save)

--- tanstack2025/vitest-example/CardEditor.test.tsx
+++ tanstack2025/vitest-example/CardEditor.test.tsx
@@ -15,4 +15,16 @@ test("creates and saves new card", async () => {
       <CardEditor />
     </QueryClientProvider>,
   );
+
+  const saveButton = screen.getByRole("button", { name: /save/i });
+
+  await expect.element(saveButton).toBeDisabled();
+
+  await screen.getByLabelText(/title/i).fill("Your card");
+  await screen.getByLabelText(/message/i).fill("Happy testing");
+  await screen.getByAltText(/01/).click();
+  await expect.element(saveButton).toBeEnabled();
+
+  // SAVE FUNKTIONIERT (NOCH) NICHT, WEIL API-BACKEND NICHT LÄUFT
+  // await saveButton.click();
 });
