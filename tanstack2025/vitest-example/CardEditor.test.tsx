32046662e6133a5affd828a4e30120a6cc378322
Nils Hartmann <nils@nilshartmann.net>
2025-05-04 00:26:55 +0200

    [Vitest] 02: MSW zum Speichern mit Dummy-Response

--- tanstack2025/vitest-example/CardEditor.test.tsx
+++ tanstack2025/vitest-example/CardEditor.test.tsx
@@ -1,20 +1,24 @@
 import { expect, test, vi } from "vitest";
 import { render } from "vitest-browser-react";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { HttpResponse, http } from "msw";
-import { worker } from "./setup-msw.ts";
-import { newCard } from "./test-data.ts";
+import { setupApiMock } from "./setup-msw.ts";
 import CardEditor from "@/components/CardEditor.tsx";
 import { createQueryClient } from "@/router.tsx";
 
 import "../src/globals.css";
 
 test("creates and saves new card", async () => {
+  await setupApiMock();
+
   const screen = render(
     <QueryClientProvider client={createQueryClient()}>
       <CardEditor />
     </QueryClientProvider>,
   );
+
+  await expect
+    .element(screen.getByText(/Create your personal greeeting card/i))
+    .toBeInTheDocument();
 
   const saveButton = screen.getByRole("button", { name: /save/i });
 
@@ -26,5 +30,6 @@ test("creates and saves new card", async () => {
   await expect.element(saveButton).toBeEnabled();
 
   // SAVE FUNKTIONIERT (NOCH) NICHT, WEIL API-BACKEND NICHT LÄUFT
-  // await saveButton.click();
+  await saveButton.click();
+  await expect.element(screen.getByText(/New card saved/i)).toBeInTheDocument();
 });
